74b66e8bda894f471b9ed7e3b8166469692cded2
Stanimir Dimitrov <skdimitrov@yahoo.com>
2016-08-18 10:12:51 +0300

    add
    add

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('parts.doctype')
+@include('parts.navbarLog')
+
+@section('page-content')
+
+    <div class="jumbotron">
+        <h1 class="text-center">Lorem Ipsum</h1>
+        <p>
+            Lorem ipsum dolor sit amet, posuere fermentum sed nibh morbi dolor orci, lorem tellus volutpat ac
+            suspendisse, orci aliquam aliquam, amet ut nunc diam. Impedit dolor congue quisque sit porta, tristique
+            enim tristique luctus tincidunt adipiscing ac. Pellentesque quis praesent. Vestibulum pulvinar ligula
+            bibendum in. Erat suspendisse felis lorem, sapien aenean, condimentum quam ea donec felis. Orci viverra
+            vestibulum, non eget dui placerat scelerisque, aliquam diam est sit elit, pellentesque quis. Mi sed integer
+            quam, erat ut nisl duis adipiscing, augue inceptos ac sed turpis, amet nec arcu sagittis venenatis. Risus
+            a ut quam est mattis. Enim integer pulvinar dictum est.
+        </p>
+        <p>
+            Convallis enim ut. Non nulla risus non, reprehenderit habitant potenti. Scelerisque lorem. Laborum
+            gravida odio sed est eros. Cras reprehenderit sagittis elit porta id, iaculis mauris. Tempus amet eget
+            porta id sociis, non viverra sed, a nulla, amet erat dolor, lorem facilisis malesuada suspendisse etiam
+            lectus suspendisse. Arcu scelerisque malesuada wisi, ante lectus lacus mattis justo adipiscing, fringilla
+            sapien morbi praesent vestibulum culpa proin.
+        </p>
+        <p>
+            След като се регитрирате и влезнете ще се запознаете с идеята
+            на проекта. В менютата ще имате лична страничка и достъп до други страници.
+            Разполагаме и с админ панел :).
+        </p>
+    </div>
+
+
+    @stop
